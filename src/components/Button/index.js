f68bca84e665d89dceade9e64170b33f55da1244
Gabriela Melo <gabry_mello@hotmail.com>
2020-08-02 18:04:39 -0300

    Setup inicial do projeto

---
+++ src/components/Button/index.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    
+    color: var(--white);
+    border: 4px double var(--white);
+    box-sizing: border-box;
+    cursor: pointer;
+    padding: 16px 24px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    outline: none;
+    border-radius: 4px;
+    text-decoration: none;
+    display: inline-block;
+    transition: opacity .3s;
+
+    &:hover,
+    &:focus {
+        opacity: .5;
+        box-shadow: 0 0 30px yellow;
+        background: yellow;
+        color: black;
+    }
+`;
+
+export default Button;
